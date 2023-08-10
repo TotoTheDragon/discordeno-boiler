d6c0aa5aa28370605945b1fe2087049989612ffe
TotoTheDragon <thomasdvandeweerd@gmail.com>
2023-08-10 03:41:32 +0200

    Add some type stuff for param parsing and neater object

--- src/structure/typeUtil.ts
+++ src/structure/typeUtil.ts
@@ -1,8 +1,18 @@
 export type Mergify<TObj> = {
     [Key in keyof TObj]: TObj[Key];
-  } & unknown; // Adding `& unknown` forces TS to "resolve" the type
+} & unknown; // Adding `& unknown` forces TS to "resolve" the type
 
-  export type AddProperty<A extends object, Key extends string, ReturnType, Required extends boolean> =
-  Required extends true 
-  ? Mergify<A & {[property in Key]: ReturnType}>
-  : Mergify<A & {[property in Key]?: ReturnType}>
+export type AddProperty<A extends object, Key extends string, ReturnType, Required extends boolean> =
+    Required extends true
+    ? Mergify<A & { [property in Key]: ReturnType }>
+    : Mergify<A & { [property in Key]?: ReturnType }>
+
+export type IsParameter<Part> = Part extends `:${infer ParamName}` ? ParamName : never;
+export type FilteredParts<Path> = Path extends `${infer PartA}/${infer PartB}`
+    ? IsParameter<PartA> | FilteredParts<PartB>
+    : IsParameter<Path>;
+export type Params<Path> = {
+    [Key in FilteredParts<Path>]: string;
+};
+
+export type KeyValueMap = {[key: string]: any};
